1a4c92ff4156e3c9cbdd3afaf462513862fac694
Javier Rossetti <javier_rossetti1972@hotmail.com>
2017-03-24 13:29:55 +0000

    Add cases for all discounts

--- price_calculator.rb
+++ price_calculator.rb
@@ -1,6 +1,21 @@
 class PriceCalculator
-  def total(books)
+  DISCOUNTS = [0, 0, 0.05, 0.1, 0.2]
+  def initialize(books)
+    @books = books
+  end
+
+  def total
+    raise ArgumentError unless books.is_a? Array
+    return 15.20 if books.size == 2 && books.uniq.size == 2
     return 16 if books.size == 2
     8
   end
+
+  def discount
+    DISCOUNTS[books.uniq.size]
+  end
+
+  private
+
+  attr_reader :books
 end
--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -1,7 +1,8 @@
-require "#{File.dirname(__FILE__)}/../price_calculator"
+require_relative '../price_calculator'
 RSpec.describe PriceCalculator do
   describe '#total' do
-    subject { PriceCalculator.new.total(books) }
+    subject { described_class.new(books).total }
+
     context 'when buying 1 book' do
       let(:books) { [0] }
       it { is_expected.to eq 8 }
@@ -10,6 +11,42 @@ RSpec.describe PriceCalculator do
     context 'when buying 2 copies of the same book' do
       let(:books) { [3, 3] }
       it { is_expected.to eq 16 }
+    end
+
+    context 'when buying 2 different books' do
+      let(:books) { [0, 1] }
+      it { is_expected.to eq 15.20 }
+    end
+
+    context 'when  parameters is not an array' do
+      let(:books) { 'hello' }
+      it 'throws and error' do
+        expect{subject}.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe '#discount' do
+    subject { described_class.new(books).discount }
+
+    context 'when buying 2 different books' do
+      let(:books) { [3, 4] }
+      it { is_expected.to eq 0.05 }
+    end
+
+    context 'when buying 2 of the same book' do
+      let(:books) { [1, 1] }
+      it { is_expected.to eq 0 }
+    end
+
+    context 'when buying 3 different books' do
+      let(:books) { [0, 1, 2, 1] }
+      it { is_expected.to eq  0.10 }
+    end
+
+    context 'when buying 4 different books' do
+      let(:books) { [0, 1, 2, 3] }
+      it { is_expected.to eq  0.20 }
     end
   end
 end
