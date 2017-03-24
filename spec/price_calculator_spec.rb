714c1b0f9238a43bef8504bfe1cf90e752941819
Javier Rossetti <javier_rossetti1972@hotmail.com>
2017-03-24 11:47:30 +0000

    First commit

---
+++ spec/price_calculator_spec.rb
@@ -0,0 +1,15 @@
+require "#{File.dirname(__FILE__)}/../price_calculator"
+RSpec.describe PriceCalculator do
+  describe '#total' do
+    subject { PriceCalculator.new.total(books) }
+    context 'when buying 1 book' do
+      let(:books) { [0] }
+      it { is_expected.to eq 8 }
+    end
+
+    context 'when buying 2 copies of the same book' do
+      let(:books) { [3, 3] }
+      it { is_expected.to eq 16 }
+    end
+  end
+end
